f238911c8aa340a99db0cb7445546290c5ca9d01
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-04-08 11:56:28 +0000

    [UPDATE] renommage du dossier icons en changeicons [NEW] Ajout de DOCUMENT_ROOT

--- webapp/bin/tasks/plannedTasks.php
+++ webapp/bin/tasks/plannedTasks.php
@@ -1,7 +1,7 @@
 <?php
 // In apache user's crontab, please add:
 // # run hourChange task every hour
-// */5 * * * * php ${WEBEDIT_HOME}/webapp/bin/tasks/plannedTasks.php
+// */5 * * * * php ${WEBEDIT_HOME}/bin/tasks/plannedTasks.php
 
 require_once("BaseTask.php");
 
@@ -14,11 +14,19 @@ class f_tasks_PlannedTasksTask extends f_tasks_BaseTask
 
 	protected function execute($previousRunTime)
 	{
+		
 		$this->loadFramework();
 		Controller::newInstance("controller_ChangeController");
 		task_PlannedTaskRunner::main();
 	}
+	
+	function startNoLock()
+	{
+		$this->checkWebeditHome();
+		chdir(WEBEDIT_HOME);
+		$this->execute(null);
+	}
 }
 
 $task = new f_tasks_PlannedTasksTask();
-$task->start();
+$task->startNoLock();
--- webapp/bin/tasks/runner.php
+++ webapp/bin/tasks/runner.php
@@ -1,25 +1,11 @@
 <?php
 if (!defined('WEBEDIT_HOME'))
 {
-	// Install path is webapp/bin/tasks/<myTaskPath>.php
-	if (!isset($_SERVER["PWD"]) || !isset($_SERVER["SCRIPT_FILENAME"]))
-	{
-		throw new Exception("Could not discover WEBEDIT_HOME: PWD = " . var_export($_SERVER["PWD"]) . ", SCRIPT_FILENAME = " . var_export($_SERVER["SCRIPT_FILENAME"]));
-	}
-	if ($_SERVER["SCRIPT_FILENAME"][0] == '/')
-	{
-		$thisPath = $_SERVER["SCRIPT_FILENAME"];
-	}
-	else
-	{
-		$thisPath = $_SERVER["PWD"] . DIRECTORY_SEPARATOR . $_SERVER["SCRIPT_FILENAME"];
-	}
-	define('WEBEDIT_HOME', realpath(dirname($thisPath) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..'));
+	define('WEBEDIT_HOME', dirname(dirname(dirname(realpath(__FILE__)))));
 }
 
 chdir(WEBEDIT_HOME);
-
-require_once WEBEDIT_HOME . DIRECTORY_SEPARATOR . 'framework' . DIRECTORY_SEPARATOR . 'Framework.php';
+require_once WEBEDIT_HOME . '/framework/Framework.php';
 $rq = RequestContext::getInstance();
 $rq->setLang($rq->getDefaultLang());
 Controller::newInstance("controller_ChangeController");
