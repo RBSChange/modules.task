1bdb943aa3f57214a4a847794216990ce083a7d7
Gaël PORT <gael.port@rbs.fr>
2010-04-08 07:20:57 +0000

    [NEW] Added method TaskHelper::getTaskForUserIdByDocumentId

--- lib/helpers/TaskHelper.class.php
+++ lib/helpers/TaskHelper.class.php
@@ -34,7 +34,7 @@ class TaskHelper
 	public static function getPendingTasksForCurrentUser()
 	{
 		$query = f_persistentdocument_PersistentProvider::getInstance()->createQuery('modules_task/usertask');
-		$query->add(Restrictions::eq('user', Controller::getInstance()->getContext()->getUser()->getId()));
+		$query->add(Restrictions::eq('user', users_UserService::getInstance()->getCurrentUser()));
 		$query->add(Restrictions::published());
 		$query->addOrder(Order::desc('document_creationdate'));
 		$query->setMaxResults(50);
@@ -48,11 +48,27 @@ class TaskHelper
 	public static function getPendingTasksForCurrentUserByDocumentId($documentId)
 	{
 		$query = f_persistentdocument_PersistentProvider::getInstance()->createQuery('modules_task/usertask');
-		$query->add(Restrictions::eq('user', Controller::getInstance()->getContext()->getUser()->getId()));
+		$query->add(Restrictions::eq('user', users_UserService::getInstance()->getCurrentUser()));
 		$query->add(Restrictions::published());
 		$query->add(Restrictions::eq('workitem.documentid', $documentId));
 		$query->addOrder(Order::desc('document_creationdate'));
 		$query->setMaxResults(50);
 		return $query->find();
+	}
+
+	/**
+	 * @param users_persistentdocument_user $user
+	 * @param Integer $documentId
+	 * @return array<task_persistentdocument_usertask>
+	 */
+	public static function getTaskForUserIdByDocumentId($user, $documentId, $taskId)
+	{
+		$query = task_UsertaskService::getInstance()->createQuery();
+		$query->add(Restrictions::published());
+		$query->add(Restrictions::eq('user', $user));
+		$criteria = $query->createCriteria('workitem');
+		$criteria->add(Restrictions::eq('documentid', $documentId));
+		$criteria->add(Restrictions::eq('transition.taskid', $taskId));
+		return $query->findUnique();
 	}
 }
