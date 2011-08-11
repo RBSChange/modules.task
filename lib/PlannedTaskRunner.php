78e27c866f5fd0afc5b5b7718e26ad4c09e1239f
Franck STAUFFER <franck.stauffer@rbs.fr>
2011-08-11 12:21:44 +0200

    ZF Rewrite

--- lib/PlannedTaskRunner.php
+++ lib/PlannedTaskRunner.php
@@ -102,19 +102,15 @@ class task_PlannedTaskRunner
 	 */
 	public static function launchTask($URL)
 	{
-		//Framework::info(__METHOD__ . ' ' . $URL);
+		Framework::info(__METHOD__ . ' ' . $URL);
 		
-		$rc = curl_init();
-		curl_setopt($rc, CURLOPT_RETURNTRANSFER, false);
-		curl_setopt($rc, CURLOPT_HTTPHEADER, array('Cache-Control: no-store, no-cache, no-transform', 'Pragma: no-cache', 'Connection: close'));
-		curl_setopt($rc, CURLOPT_USERAGENT, 'RBSChange/3.0');
-		curl_setopt($rc, CURLOPT_POST, 1);
-		curl_setopt($rc, CURLOPT_POSTFIELDS, null);	
-		curl_setopt($rc, CURLOPT_TIMEOUT, 5);
-		curl_setopt($rc, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($rc, CURLOPT_URL, $URL);
-		curl_exec($rc);
-		curl_close($rc);
+		$client = change_HttpClientService::getInstance()->getNewHttpClient(array('timeout' => 5));
+		$client->setHeaders(array('Cache-Control: no-store, no-cache, no-transform', 'Pragma: no-cache', 'Connection: close'));
+		$client->setUri($URL);
+		
+		// we ignore server response
+		$client->setStream(null);
+		$client->request(Zend_Http_Client::POST);
 	}
 	
 	/**
@@ -122,20 +118,13 @@ class task_PlannedTaskRunner
 	 */
 	public static function pingChangeCronURL($pingURL)
 	{
-		//Framework::info(__METHOD__ . ' ' . $pingURL);
+		$client = change_HttpClientService::getInstance()->getNewHttpClient(array('timeout' => 1));
+		$client->setHeaders(array('Cache-Control: no-store, no-cache, no-transform', 'Pragma: no-cache', 'Connection: close'));
+		$client->setUri($pingURL);
 		
-		$rc = curl_init();
-		curl_setopt($rc, CURLOPT_RETURNTRANSFER, false);
-		curl_setopt($rc, CURLOPT_HTTPHEADER, array('Cache-Control: no-store, no-cache, no-transform', 'Pragma: no-cache', 'Connection: close'));
-		curl_setopt($rc, CURLOPT_USERAGENT, 'RBSChange/3.0');
-		curl_setopt($rc, CURLOPT_POST, 1);
-		curl_setopt($rc, CURLOPT_POSTFIELDS, null);
-		curl_setopt($rc, CURLOPT_FRESH_CONNECT, 1); // don't use a cached version of the url 	
-		curl_setopt($rc, CURLOPT_TIMEOUT, 1);
-		curl_setopt($rc, CURLOPT_FOLLOWLOCATION, 0);
-		curl_setopt($rc, CURLOPT_URL, $pingURL);
-		curl_exec($rc);
-		curl_close($rc);
+		// we ignore server response
+		$client->setStream(null);
+		$client->request(Zend_Http_Client::POST);
 	}	
 	
 	/**
