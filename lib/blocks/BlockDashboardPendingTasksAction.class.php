cc456d999e49428aff1ab000390883fb8db55f71
Gaël PORT <gael.port@rbs.fr>
2012-04-24 17:54:40 +0200

    [NEW] Homogenize dates format in dashboard widgets.

--- lib/blocks/BlockDashboardPendingTasksAction.class.php
+++ lib/blocks/BlockDashboardPendingTasksAction.class.php
@@ -29,23 +29,13 @@ class task_BlockDashboardPendingTasksAction extends dashboard_BlockDashboardActi
 				Framework::warn(__METHOD__ . ' no document found with id ' . $task->getWorkitem()->getDocumentid() .  ' for the task with id ' . $task->getId());
 				continue;
 			}
-			
-			$lastModification = date_Calendar::getInstance($task->getCreationdate());
 
-			if ($lastModification->isToday())
-			{
-				$status = f_Locale::translateUI('&modules.uixul.bo.datePicker.Calendar.today;') . date_DateFormat::format(date_Converter::convertDateToLocal($lastModification), ', H:i');
-			}
-			else
-			{
-				$status = date_DateFormat::format(date_Converter::convertDateToLocal($lastModification), 'l j F Y, H:i');
-			}
 			$attr = array(
 				'id' => $task->getId(),
 				'taskLabel' => $task->getLabelAsHtml(),
 				'dialog' => $task->getDialogName(),
 				'module' => $task->getModule(),
-				'status' => ucfirst($status),
+				'status' => date_Formatter::toDefaultDateTimeBO($task->getUICreationdate()),
 				'documentId' => $document->getId(),
 				'documentLabel' => f_util_HtmlUtils::textToHtml($document->getPersistentModel()->isLocalized() ? $document->getLabelForLang($task->getLang()) : $document->getLabel()),
 				'documentModel' => str_replace('/', '_', $document->getDocumentModelName()),
