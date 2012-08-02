c768489680c8b9ecb7608431168cecbbd4002de4
Loïc Couturier <loic.couturier@rbs.fr>
2012-08-02 15:54:34 +0200

    [UPDATE] Fix action in perspective. Clean bad comment

--- changedev-commands/task_UnlockAll.php
+++ changedev-commands/task_UnlockAll.php
@@ -20,31 +20,6 @@ class commands_task_UnlockAll extends commands_AbstractChangeCommand
 	{
 		return "Unlock all tasks --reset to unlock and rerun tasks --show to only view which tasks is locked";
 	}
-	
-	/**
-	 * This method is used to handle auto-completion for this command.
-	 * @param Integer $completeParamCount the parameters that are already complete in the command line
-	 * @param String[] $params
-	 * @param array<String, String> $options where the option array key is the option name, the potential option value or true
-	 * @return String[] or null
-	 */
-//	function getParameters($completeParamCount, $params, $options, $current)
-//	{
-//		$components = array();
-//		
-//		// Generate options in $components.		
-//		
-//		return array_diff($components, $params);
-//	}
-	
-	/**
-	 * @param String[] $params
-	 * @param array<String, String> $options where the option array key is the option name, the potential option value or true
-	 * @return boolean
-	 */
-//	protected function validateArgs($params, $options)
-//	{
-//	}
 
 	/**
 	 * @return String[]
